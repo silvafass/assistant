f52cb043c682276dc79217aa934b299ff4255349
silvafass <silva.fass@gmail.com>
2026-06-10 23:57:01 -0300

    feat: add support streaming responses from the Ollama API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use anyhow::Result;
 use serde_json::{Value, json};
 
@@ -8,18 +10,25 @@ async fn main() -> Result<()> {
     let payload = json!({
         "model": "gemma4",
         "prompt": "Why is the sky blue?",
-        "stream": false,
+        "stream": true,
     });
 
-    let response = client
+    let mut response = client
         .post("http://localhost:11434/api/generate")
         .json(&payload)
         .send()
-        .await?
-        .json::<Value>()
         .await?;
 
-    dbg!(response);
+    if payload["stream"].as_bool().unwrap() {
+        while let Some(chunk) = response.chunk().await? {
+            let chunk: Value = serde_json::from_slice(&chunk)?;
+            print!("{}", chunk["response"].as_str().unwrap());
+            std::io::stdout().flush()?
+        }
+    } else {
+        let response = response.json::<Value>().await?;
+        println!("{}", response["response"].as_str().unwrap())
+    }
 
     Ok(())
 }
